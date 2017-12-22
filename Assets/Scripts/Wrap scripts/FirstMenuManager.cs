873256d592763f1a25c20c9cbd1a13ca55ead6e7
hatzv <hatzvika@gmail.com>
2017-12-22 14:41:39 +0800

    Wired the win/draw/lose menus in the start screen, to replace the game over scenes from before.

---
+++ Assets/Scripts/Wrap scripts/FirstMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class FirstMenuManager
+{
+
+
+	private static string firstMenuName = "Main Interface";
+
+	public static string GetFirstMenuName ()
+	{
+		return firstMenuName;
+	}
+
+	public static void SetFirstMenuName(string menuName){
+		firstMenuName = menuName;
+	}
+}
---
+++ Assets/Scripts/Wrap scripts/ShowFirstMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFirstMenu : MonoBehaviour {
+
+	void Start () {
+		string menuToShow = FirstMenuManager.GetFirstMenuName ();
+		foreach (Transform child in transform){
+			if (child.name == menuToShow){
+				child.gameObject.SetActive (true);
+			} else {
+				child.gameObject.SetActive (false);
+			}
+		}
+
+		// Return to showing first the main interface after a win/draw/lose screen was shown first
+		FirstMenuManager.SetFirstMenuName ("Main Interface");
+	}
+	
+}
